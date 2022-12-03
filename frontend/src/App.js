c86c14c196bf7ed1fc613a5481becf3610ca1874
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-12-03 17:41:25 -0300

    Index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import GlobalStyle from "./styles/global.js"
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Container>
+        <Title>PARTIDAS</Title>
+        <Form onEdit={onEdit} setOnEdit={setOnEdit} getUsers={getUsers} />
+        <Grid setOnEdit={setOnEdit} users={users} setUsers={setUsers} />
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyle />
+    </>
   );
 }
 
