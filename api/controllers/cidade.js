ee26e0e6cf108a2b62da74c54a81b1968c8eee89
guiosouza <guilhermedeoliveiradesouza@gmail.com>
2022-12-05 18:04:54 -0300

    CREATE cidade

--- api/controllers/cidade.js
+++ api/controllers/cidade.js
@@ -4,8 +4,26 @@ export const getCidade = (_, res) => {
     const q = "SELECT * FROM cidade"
 
     db.query(q, (err, data) => {
-        if(err) return res.json(err);
+        if (err) return res.json(err);
 
         return res.status(200).json(data);
+    });
+};
+
+
+export const addCidade = (req, res) => {
+    const q =
+        "INSERT INTO (`idcidade`, `nome`, `time_idtime`) VALUES(?)";
+
+    const values = [
+        req.body.idcidade,
+        req.body.nome,
+        req.body.time_idtime,
+    ];
+
+    db.query(q, [values], (err) => {
+        if (err) return res.json(err);
+
+        return res.status(200).json("Cidade cadastrada com sucesso.");
     });
 };
